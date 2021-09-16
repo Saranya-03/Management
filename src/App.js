9ce48a9a9aaae67a85f02234b382548890809300
Saranya-03 <61792945+Saranya-03@users.noreply.github.com>
2021-09-17 00:07:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from "./Pages/Home/home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./app.css";
-import Topbar from "./Components/Topbar/topbar";
-import SalesPerson from "./Pages/SalesPerson/salesPerson";
-const { default: Sidebar} = require("./Components/Sidebar/sidebar");
+import Topbar from "./components/Topbar/Topbar";
+
+import ManagementSidebar from "./components/managementSidebar/Sidebar";
+import ManagementHome from "./pages/management/Home/managementHome";
+import Orders from "./pages/management/Orders/orders";
+import Products from "./pages/management/Products/products";
+import AddProducts from "./pages/management/Products/addProduct";
+import Reports from "./pages/management/Reports/reports";
+import Routes from "./pages/management/Route/route";
+import SalesPerson from "./pages/management/SalesPerson/salesPerson";
+import AddSalesPerson from "./pages/management/SalesPerson/NewSalesPerson";
+import Shops from "./pages/management/Shops/shops";
 
 
 function App() {
   return (
+    <Router>
     <div className="App"> 
       <Topbar />
       <div className="container">
-        <Sidebar />
-        <BrowserRouter>
+        <ManagementSidebar />
+        
           <Switch>
-            <Route exact path="/">
-              <Home />
+            <Route exact path="/management">
+              <ManagementHome/>
+            </Route>  
+
+            <Route exact path="/management/salesperson">
+              <SalesPerson/>
             </Route>
-            <Route exact path="/SalesPerson">
-              <SalesPerson />
+
+            <Route path="/management/salesperson/addSalesPerson">
+              <AddSalesPerson/>
             </Route>
-          </Switch>  
-        </BrowserRouter>
-              
+
+            <Route exact path="/management/products">
+              <Products/>
+            </Route>  
+
+            <Route path="/management/products/addProduct">
+              <AddProducts/>
+            </Route>  
+            
+            <Route path="/management/orders">
+              <Orders/>
+            </Route>  
+            
+            <Route path="/management/shops">
+              <Shops/>
+            </Route> 
+            
+            <Route path="/management/routes">
+              <Routes/>
+            </Route>  
+            
+            <Route path="/management/reports">
+              <Reports/>
+            </Route> 
+             
+          </Switch>   
       </div>
-    </div>
       
+    </div>
+    </Router> 
   );
 }
 
